4ab55924c33e9e792c0416c9e9fe77e330814635
unknown <yardenshaham10@gmail.com>
2021-09-24 17:49:09 +0300

    Gig Details Link CHECK

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import {HomePage} from './pages/home-page.jsx'
 import {GigApp} from './pages/gig-app.jsx'
 import { ExplorePage } from './pages/explore-page.jsx';
+// import{}from
 
 // Routes accesible from the main navigation (in AppHeader)
 const routes = [
@@ -19,6 +20,14 @@ const routes = [
         component: ExplorePage,
         label: 'Explore'
     },
+    {
+        path:'/gig/:gigId',
+        component: GigDetails,
+        label: 'Details'
+    },
+
+    
+
 ]
 
 export default routes;
