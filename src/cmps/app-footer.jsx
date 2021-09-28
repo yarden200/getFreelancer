abdfaa62f09d3d146baa6e4d9e5135d649b701be
Itay Azani <itay.azani@gmail.com>
2021-09-28 22:45:02 +0300

    itay 28sep 22:44

--- src/cmps/app-footer.jsx
+++ src/cmps/app-footer.jsx
@@ -1,10 +1,17 @@
 
 import React from 'react'
-import { connect } from 'react-redux'
+// import { connect } from 'react-redux'
 
 // import { showSuccessMsg } from '../services/event-bus.service'
-import { addToCart, removeFromCart, checkout } from '../store/gig.actions'
-import { UserMsg } from './user-msg.jsx'
+// import { addToCart, removeFromCart, checkout } from '../store/gig.actions'
+// import { UserMsg } from './user-msg.jsx'
+import { FaTwitter } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaPinterest } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+
+
 
 
 class _AppFooter extends React.Component {
@@ -15,26 +22,43 @@ class _AppFooter extends React.Component {
 
     componentDidMount() { }
 
-    removeFromCart = (carId) => {
-        this.props.removeFromCart(carId)
-    }
+    // removeFromCart = (carId) => {
+    //     this.props.removeFromCart(carId)
+    // }
 
-    checkout = () => {
-        this.props.checkout();
-    }
-    
-    get cartTotal() {
-        return this.props.cart.reduce((acc, gig) => acc + gig.price, 0)
-    }
+    // checkout = () => {
+    //     this.props.check
+    // out();
+    // }
+
+    // get cartTotal() {
+    //     return this.props.cart.reduce((acc, gig) => acc + gig.price, 0)
+    // }
 
     render() {
-        const { isCartShown } = this.state
-        const { count, cart } = this.props;
+        // const { isCartShown } = this.state
+        // const { count, cart } = this.props;
         return (
             <footer className="app-footer">
-                <p>
-                    coffeerights 2021 - count: {count}
-                </p>
+                <div className="footer-bottom-line">
+                    <div className="footer-left">
+
+                        <div className="footer-logo">finderr<span>®️</span></div>
+                        <div className="ltd">
+                            ©️ finderr international Ltd. 2021
+                        </div>
+                    </div>
+                        <div className="footer-right">
+
+                            <h3><FaTwitter /></h3>
+                            <h3><FaFacebook /></h3>
+                            <h3><FaLinkedin /></h3>
+                            <h3><FaPinterest /></h3>
+                            <h3><FaInstagram /></h3>
+                        </div>
+                </div>
+
+                {/* {count}
                 {cart.length > 0 &&
                     <h5>
                         <span>{cart.length}</span> Products in your Cart
@@ -61,24 +85,24 @@ class _AppFooter extends React.Component {
                     <p>Total: ${this.cartTotal.toLocaleString()} </p>
                     <button onClick={this.checkout}>Checkout</button>
                 </section>}
-                <UserMsg />
+                <UserMsg /> */}
             </footer>
         )
     }
 }
 
 
-function mapStateToProps(state) {
-    return {
-        count: state.userModule.count,
-        cart: state.gigModule.cart
-    }
-}
+// function mapStateToProps(state) {
+//     return {
+//         count: state.userModule.count,
+//         cart: state.gigModule.cart
+//     }
+// }
 
-const mapDispatchToProps = {
-    checkout,
-    addToCart,
-    removeFromCart
-}
+// const mapDispatchToProps = {
+//     checkout,
+//     addToCart,
+//     removeFromCart
+// }
 
-export const AppFooter = connect(mapStateToProps, mapDispatchToProps)(_AppFooter)
+export const AppFooter = /*connect(mapStateToProps, mapDispatchToProps)*/(_AppFooter)
